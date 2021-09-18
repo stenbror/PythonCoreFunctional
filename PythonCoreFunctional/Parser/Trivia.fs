06a4e6649285791abe9da0fdcc4f2a5dd9a5f185
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-09-18 16:39:54 +0200

    Trivia done

--- PythonCoreFunctional/Parser/Trivia.fs
+++ PythonCoreFunctional/Parser/Trivia.fs
@@ -3,3 +3,9 @@ namespace PythonCore.Runtime.Parser
 
 type Trivia = 
     |   Empty
+    |   Whitespace of int * int
+    |   Tabulator of int * int
+    |   Newline of int * int * char * char
+    |   LineContinuation of int * int
+    |   Comment of int * int * string
+    
