3eecde518fd84cd97b031d0b36a370dab0abe058
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-09-19 12:17:07 +0200

    Added Node for Comparison

--- PythonCoreFunctional/Parser/Node.fs
+++ PythonCoreFunctional/Parser/Node.fs
@@ -7,4 +7,17 @@ type Node =
     |   Test of uint32 * uint32 * Node * Token * Node * Token * Node
     |   Lambda of uint32 * uint32 * Token * Node * Token * Node * bool
     |   OrTest of uint32 * uint32 * Node * Token * Node 
+    |   AndTest of uint32 * uint32 * Node * Token * Node 
+    |   NotTest of uint32 * uint32 * Token * Node 
+    |   Less of uint32 * uint32 * Node * Token * Node 
+    |   Greater of uint32 * uint32 * Node * Token * Node 
+    |   Equal of uint32 * uint32 * Node * Token * Node 
+    |   GreaterEqual of uint32 * uint32 * Node * Token * Node 
+    |   LessEqual of uint32 * uint32 * Node * Token * Node 
+    |   NotEqual of uint32 * uint32 * Node * Token * Node 
+    |   In of uint32 * uint32 * Node * Token * Node 
+    |   NotIn of uint32 * uint32 * Node * Token * Node 
+    |   Is of uint32 * uint32 * Node * Token * Node 
+    |   IsNot of uint32 * uint32 * Node * Token * Node 
+    
     
