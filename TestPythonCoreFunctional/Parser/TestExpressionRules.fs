115d103a5e0de53698c589983f62c07c2ae1a248
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-10-03 20:01:27 +0200

    Added UnitTests for legacy not equal operator.

--- TestPythonCoreFunctional/Parser/TestExpressionRules.fs
+++ TestPythonCoreFunctional/Parser/TestExpressionRules.fs
@@ -716,3 +716,30 @@ module TestParserExpressionRules =
                             Token.NotEqual(7ul, 9ul, [| Trivia.WhiteSpace(6ul, 7ul) |] ),
                             Node.Number(10ul, 11ul, Token.Number(10ul, 11ul, [| Trivia.WhiteSpace(9ul, 10ul) |], "6" ))
                         ), node)
+
+    [<Fact>]
+    let ``Test comparison rule with single legacy not equel operators``() = 
+        let mutable state = ParseState.Init
+        let stream = "4 <> 5".ToCharArray() |> Tokenizer.TokenizeFromCharArray
+        let node, rest = Expressions.ParseComparison(stream, &state)
+        Assert.Equal([| Token.Eof(6ul, [| |]) |], rest)
+        Assert.Equal(Node.NotEqual(0ul, 6ul, 
+                            Node.Number(0ul, 2ul, Token.Number(0ul, 1ul, [| |], "4" )),
+                            Token.NotEqual(2ul, 4ul, [| Trivia.WhiteSpace(1ul, 2ul) |] ),
+                            Node.Number(5ul, 6ul, Token.Number(5ul, 6ul, [| Trivia.WhiteSpace(4ul, 5ul) |], "5" ))
+                        ), node)
+
+    [<Fact>]
+    let ``Test comparison rule with multiple legacy not equal operators``() = 
+        let mutable state = ParseState.Init
+        let stream = "4 <> 5 <> 6".ToCharArray() |> Tokenizer.TokenizeFromCharArray
+        let node, rest = Expressions.ParseComparison(stream, &state)
+        Assert.Equal([| Token.Eof(11ul, [| |]) |], rest)
+        Assert.Equal(Node.NotEqual(0ul, 11ul,
+                            Node.NotEqual(0ul, 7ul, 
+                                Node.Number(0ul, 2ul, Token.Number(0ul, 1ul, [| |], "4" )),
+                                Token.NotEqual(2ul, 4ul, [| Trivia.WhiteSpace(1ul, 2ul) |] ),
+                                Node.Number(5ul, 7ul, Token.Number(5ul, 6ul, [| Trivia.WhiteSpace(4ul, 5ul) |], "5" )) ),
+                            Token.NotEqual(7ul, 9ul, [| Trivia.WhiteSpace(6ul, 7ul) |] ),
+                            Node.Number(10ul, 11ul, Token.Number(10ul, 11ul, [| Trivia.WhiteSpace(9ul, 10ul) |], "6" ))
+                        ), node)
